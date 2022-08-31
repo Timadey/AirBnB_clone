8b1477ac5087932c025d61885ee331e6719491ef
Timadey <dlktimothy@gmail.com>
2022-08-31 22:57:55 +0100

    Fix key __class__ of to_dict method in base_model

--- models/base_model.py
+++ models/base_model.py
@@ -26,7 +26,6 @@ class BaseModel():
         Initializes the BaseModel
         """
         import uuid
-        from datetime import datetime
         self.id = str(uuid.uuid4())
         self.created_at = datetime.now()
         self.updated_at = self.created_at
@@ -58,7 +57,7 @@ class BaseModel():
         """
 
         class_dict = self.__dict__.copy()
-        class_dict['__class__'] = "BaseModel"
+        class_dict['__class__'] = __class__.__name__
         class_dict['created_at'] = datetime.isoformat(
             class_dict['created_at'])
         class_dict['updated_at'] = datetime.isoformat(
